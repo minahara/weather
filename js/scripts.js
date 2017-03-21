159c8e787dfc06a3ce359038140f4b067d53f49a
minahara <minahara@it-mmc16-m14.eastern.ewu.edu>
2017-03-21 14:28:53 -0700

    added the working parts in from codepen
    Codeine is working so far  but it won’t translate over into brackets

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,153 @@
-// YOUR SCRIPTS GO HERE
-//side bar
-$('#sidr-show-hide').sidr(
-);
+$('#sidr-show-hide').sidr();
+
+// Close from inside
 $('#close').sidr({
   method: 'close'
+})
 
-  
-});
-
-
-
+//WEATHER
+// Spokane
 $.simpleWeather({
-  location: 99004,
+    location: 99223,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('#spokane .temp').text(weather.temp);
+      $('#spokane .city').text(weather.city);
+      $('#spokane i').addClass('icon-'+ weather.code);
+      
+      // Get Condition Code
+      console.log(weather.code);
+      
+      if ( weather.code >= 0 && weather.code <= 12 ) {
+         $('body').addClass('storm');   
+      }
+      
+      if ( weather.code >= 13 && weather.code <= 18 ) {
+         $('body').addClass('snow');   
+      }
+      
+      if ( weather.code >= 19 && weather.code <= 22 ) {
+         $('body').addClass('haze');   
+      }
+      
+      if ( weather.code >= 23 && weather.code <= 24 ) {
+         $('body').addClass('wind');   
+      }
+      
+      if ( weather.code >= 25 && weather.code <= 30 ) {
+         $('body').addClass('cloud');   
+      }
+      
+      
+      if ( weather.code >= 31 && weather.code <= 36 ) {
+         $('body').addClass('sunny');   
+      }
+      
+      
+      if ( weather.code >= 37 && weather.code <= 47 ) {
+         $('body').addClass('storm');   
+      }
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Oops! Something went wrong...Spokane weather changes every 5 minutes anyway.');
+    }
   
-  unit: 'f',
+  });
+
+
+// Long Beach for SPRING BREAK 
+$.simpleWeather({
+    location: 98631,
+    unit: 'f',
+    success: function(weather) {
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('#long .temp').text(weather.temp);
+      $('#long .city').text(weather.city);
+      $('#long i').addClass('icon-'+ weather.code);
+      
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Oops! Something went wrong... Long Beach is usually mild anyway.');
+    }
   
-  success: function(weather){
-    // Check for weather
-    console.log(weather);
-    
-    // Display Weather
-    $('.temp').text(weather.temp);
-    $('.city').text(weather.city);
-    $('.state').text(weather.region);
-    $('.image img').attr('src', weather.image);
-  },
+  });
+
+
+//Location
+if ('geolocation' in navigator) {
+
+   $('#me').show(); 
+
+} else {
   
-  
-  error: function(error) {
-    console.log('Look outside.');
-  }
+  $('#me').hide();
+  $('#me').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('#me').click( function() {
+      
+    //load weather using your lat/long coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+      
+      // Check lat/long coordinates
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      
+      console.log(lat, long);
+      
+      // Send to SimpleWeather
+      getWeather( lat + ',' + long ); 
+    });
+   
 });
+
+// Get Weather
+var getWeather = function(location) {
+  
+    $.simpleWeather({
+    location: location,
+    unit: 'f',
+    success: function(weather) {
+      
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('#me .temp').text(weather.temp);
+      $('#me .city').text(weather.city);
+      $('#me i').addClass('icon-'+ weather.code);
+        
+
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('Oops! Something went wrong, take a peak outside.');
+    }
+  
+  });
+  
+};
+
+
+// Display Forecast
+//     $('#spokane .forecast figure:nth-child(1) img').attr('src', weather.forecast[0].image);
+//     $('.forecast figure:nth-child(1) figcaption').text(weather.forecast[1].text);
+//   },
+  
+  
+//   error: function(error) {
+//     console.log('Look outside.');
+//   }
+// });
